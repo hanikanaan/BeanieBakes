bdcddf1bece861261a665b7789054bb1619950d7
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-10-22 15:17:02 -0400

    Database management

--- beaniebakes/settings.py
+++ beaniebakes/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'searchresults.models'
+    'searchresults.apps.SearchresultsConfig'
 ]
 
 MIDDLEWARE = [
--- searchresults/models.py
+++ searchresults/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Recipes(models.Model):
     recipe_name = models.CharField(max_length=64)
-    recipe = models.CharField(max_length=2 ^ 32)
+    recipe = models.CharField(max_length=8196)
+    image = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.recipe_name, self.recipe
+        return self.recipe_name, self.recipe, self.image
