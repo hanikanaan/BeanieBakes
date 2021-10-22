53a1d846609c2c46116fd6357de96e4bffa56d9a
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-10-22 14:04:53 -0400

    Model instantiation

--- cakes/views.py
+++ cakes/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def base(request):
     return render(request, 'cakesbase.html')
 
+
 def list(request):
     return render(request, 'cakeslist.html')
--- cookies/views.py
+++ cookies/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def base(request):
     return render(request, 'cookiesbase.html')
 
+
 def list(request):
     return render(request, 'cookieslist.html')
