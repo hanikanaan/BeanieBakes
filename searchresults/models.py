979868b409cadfa95d88faac4895b62ae5529fe6
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-10-22 13:59:53 -0400

    Model created for recipes

--- searchresults/models.py
+++ searchresults/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipes(models.Model):
+    recipe_name = models.CharField(max_length=64)
+    recipe = models.CharField(max_length=2 ^ 32)
+
+    def __str__(self):
+        return self.recipe_name, self.recipe
