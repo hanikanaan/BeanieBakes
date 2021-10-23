41ca108a6f0f4e601d1604727c370d4c0df4afca
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-10-22 21:53:21 -0400

    Updated models.py to handle SQLite and prepare for search functionality via keywords, ingredients, prep time and recipe name.

--- searchresults/models.py
+++ searchresults/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class Recipes(models.Model):
     recipe_name = models.CharField(max_length=64)
-    recipe = models.CharField(max_length=8196)
+    keywords = models.CharField(max_length=4096)
+    recipe = models.CharField(max_length=8192)
     image = models.CharField(max_length=256)
+    ingredients = models.CharField(max_length=2048)
+    preptime = models.CharField(max_length=11)  # prep time = "XXX minutes"|"XX minutes"|"X minutes", X {0-9}
+    info = models.CharField(max_length=2048)
 
     def __str__(self):
         return self.recipe_name
